b12abc870bdf2ea8a57cdf15ee154d5b77fd4a3b
Rudolf Starosta <rudi@floetenmaus.de>
2021-04-06 19:55:15 +0200

    210406

--- src/trainer.ts
+++ src/trainer.ts
@@ -5,6 +5,28 @@
 const input = document.querySelector('input')        // get file name
 
 let qac: {q: string, a: string, c: number}[] = [{a: "x", q: "y", c: 0}]     // array with answers, questions and the counter
+let actualQuestion: number = 0                                              // hold the actual processed question
+let btnCtrl = document.createElement('button');                             // create a <button> element to control process
+let btnOK   = document.createElement('button');                             // create a <button> element in case answer was correct
+let txa     = document.createElement('textarea')                            // creat a <textarea> element
+
+// Arrange the text area
+txa.cols = 120
+txa.rows = 3 
+
+// Arrange the buttons
+btnCtrl.style.backgroundColor = 'lightgrey'
+btnCtrl.style.display         = 'none'
+
+btnOK.style.backgroundColor = 'lightgreen'
+btnOK.style.display         = 'none'
+btnOK.innerHTML             = 'Knew it :)'
+btnOK.onclick               = () => {}
+
+// Arrange document
+document.body.appendChild(txa)
+document.body.appendChild(btnCtrl)
+document.body.appendChild(btnOK)
 
 // This event listener has been implemented to identify a
 // change in the input section of the HTML code.
@@ -17,15 +39,19 @@ input!.addEventListener('change', doit)
 // doing the training and
 // writing back the status of the training
 
-async function doit(e: Event) {
-
-  await readQuestionAndAnswers().then(doTraining)
-  writeResult()
+function doit(e: Event) {
+  // Initialize next run
+  txa.innerHTML         = ''
+  actualQuestion        = 0
+  btnCtrl.style.display = 'block'
+  readQuestionsAndAnswers().
+    then(doTraining).
+    catch((e) => {console.log(e); console.log('Fehler beim Lesen der Fragen :-(')})
 
 }
 
-async function readQuestionAndAnswers() {
-  let fileContent = await new Promise((resolve, reject)=> {
+async function readQuestionsAndAnswers() {
+  let fileContent = await new Promise((resolve, reject) => {
     // Get file name
     const file = input!.files![0]
     let reader = new FileReader()
@@ -33,7 +59,9 @@ async function readQuestionAndAnswers() {
     reader.readAsText(file)
     // Event e = "file read"
     reader.onload = (e) => resolve(reader.result)
-   }
+    // Event e = "error"
+    reader.onerror = (e) => reject(e)
+  }
   )
   console.log(fileContent)
   qac = JSON.parse(<string> fileContent)
@@ -42,23 +70,60 @@ async function readQuestionAndAnswers() {
 
 function doTraining(){
   console.log('doTraining()')
-  console.log('qac[2].a = ' + qac[2].a)
+  writeResult()                              // initialize BLOB for download   
+  showQuestion()                             // Show first question
+}
 
-  // Write something to body
-  var elem = document.createElement("div")
-  var t = document.createTextNode('Some text in div')
-  elem.appendChild(t)
-  document.body.appendChild(elem)
+function showQuestion() {
+  console.log('showQuestion()')
+  if (actualQuestion < qac.length) {
+    btnOK.style.display = 'none'
+    btnCtrl.innerHTML = 'Show answer'
+    btnCtrl.onclick = showAnswer
 
+    txa.innerHTML = txa.innerHTML + 'Q: ' + qac[actualQuestion].q + ' '
+    txa.scrollTop = txa.scrollHeight       // Stay at the bottom of the text area 
+  }
+  else
+  {
+    btnCtrl.style.display = 'none'
+    btnOK.style.display = 'none'
+    txa.innerHTML = txa.innerHTML + '\nAll questions processed. Save the result.\n'
+    txa.scrollTop = txa.scrollHeight       // Stay at the bottom of the text area 
+    return
+  }
+}
+
+function showAnswer() {
+  if (actualQuestion < qac.length) {
+    btnCtrl.innerHTML = 'Did not know'
+    btnCtrl.onclick = () => {actualQuestion++; showQuestion()} // Go to next question in case the answer was not known 
+    btnOK.onclick = incrementCounter 
+    btnOK.style.display = 'inline'
+
+    txa.innerHTML = txa.innerHTML + 'A: ' + qac[actualQuestion].a + '\n'
+  }
+  else
+  {
+    alert('You should never have come here! Contact the author of this program.')
+    return
+  }
+}
+
+function incrementCounter() {
+  qac[actualQuestion].c++
+  actualQuestion++
+  showQuestion()
+  writeResult()
 }
 
 function writeResult() {
   console.log('writeResult()')
 
   // Provide qac for download
-  let resJSON = JSON.stringify(qac);
+  let resultJSON = JSON.stringify(qac);
   let link: HTMLAnchorElement = <HTMLAnchorElement> document.getElementById('downloadlink')
-  let data = new Blob([resJSON], {type: 'text/plain'})
+  let data = new Blob([resultJSON], {type: 'application/json'})
   link.href = window.URL.createObjectURL(data)
 
 }
